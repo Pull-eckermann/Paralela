3765133c9e6c86441b2abcfd1970a53f8081a5ab
ERICK ECKERMANN CARDOSO <eec18@inf.ufpr.br>
2022-12-06 17:11:56 -0300

    .

--- testes/pi_serial.c
+++ testes/pi_serial.c
@@ -7,24 +7,20 @@ static long num_steps = 100000;
 double step;
 
 int main () {
-  int i; 
-	double pi = 0.0;
-  step = 1.0/(double) num_steps;
+  	int i; 
+	double pi = 0.0, sum = 0.0;
+  	step = 1.0/(double) num_steps;
 
-	#pragma omp parallel num_threads(NUM_THREADS)
+	#pragma omp parallel
 	{
-		int i, id, nthrds;
-		double x, sum = 0.0;
-		id = omp_get_thread_num();
-		nthrds = omp_get_num_threads();
-  	for (i=id, sum = 0.0; i < num_steps; i = i + nthrds){
-  	  x = (i + 0.5) * step; // Largura do retângulo
-  	  sum = sum + 4.0 / (1.0 + x*x); // Sum += Área do retângulo
-  	}
-		#pragma omp critical
-  		pi = step * sum;
+		double x;
+		#pragma omp for reduction(+:sum)
+			for (i = 0; i < num_steps; i++){
+				x = (i+0.5)*step;
+				sum = sum + 4.0/(1.0+x*x);
+			}
 	}
-
-  printf("PI: %f\n", pi*4);
+	pi = step * sum;
+	printf("PI: %f\n", pi*4);
 } 
 
